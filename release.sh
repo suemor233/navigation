7af949131246408dc67465256adcd0ea7f6f0227
suemor <3100825062@qq.com>
2022-08-01 20:02:21 +0800

    release: v1.0.0

--- release.sh
+++ release.sh
@@ -1,3 +1,16 @@
-pnpm i
-pnpm build
-zip -r release.zip dist/*
+#!env bash
+CWD=$(pwd)
+npm run build
+cd .next
+pwd
+rm -rf cache
+cp ../next.config.js ./standalone/next.config.js
+cp -r ../public ./standalone/public
+
+cd ./standalone
+echo 'process.title = "navigation (NextJS)"' >>server.js
+mv ../static/ ./.next/static
+
+cd ..
+
+zip --symlinks -r $CWD/release.zip ./*
