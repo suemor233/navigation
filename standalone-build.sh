6f7523601656c8a333e6dd642e6391bd41e9743a
suemor <3100825062@qq.com>
2022-08-01 20:18:21 +0800

    release: v1.0.3

--- standalone-build.sh
+++ standalone-build.sh
@@ -1,16 +1,2 @@
-#!env bash
-CWD=$(pwd)
 npm run build
-cd .next
-pwd
-rm -rf cache
-cp ../next.config.js ./standalone/next.config.js
-cp -r ../public ./standalone/public
-
-cd ./standalone
-echo 'process.title = "navigation (NextJS)"' >>server.js
-mv ../static/ ./.next/static
-
-cd ..
-
-zip --symlinks -r $CWD/release.zip ./*
+zip -r release.zip .next/*
